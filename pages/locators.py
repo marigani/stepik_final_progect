f03409b5edaa854b6f87ae2bd937633e55729e94
Ganieva Marina <ganievamv@gmail.com>
2022-05-18 13:06:07 +0400

    import class BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     BUTTON_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
@@ -18,3 +17,7 @@ class ProductPageLocators():
     NAME_BOOK = (By.CSS_SELECTOR, ".product_main h1")
     COST_BOOK = (By.CSS_SELECTOR, ".product_main .price_color")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-success:nth-child(1) .alertinner")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- test_product_page.py
+++ test_product_page.py
@@ -3,7 +3,6 @@ from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 import pytest
 
-
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
@@ -34,3 +33,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open() 
     page.find_button_basket()
     page.should_be_success_message_is_disappeared()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_pagefrom_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95"
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
